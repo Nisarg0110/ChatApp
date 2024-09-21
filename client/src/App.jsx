819f226d497fea34ec7ffebcc610ef2189b0a019
nisarg kadliya <nisargkadliya@gmail.com>
2024-09-21 21:56:46 +0530

    files added

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatPage";
+
+function App() {
+  const [user, setUser] = useState();
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
